78ce7477a1ea2efac55e16f10f60586f6f44d633
git_xxx <12345678@qq.com>
2019-01-15 14:40:54 +0800

    mysql backup

--- backup/mysql_backup.sh
+++ backup/mysql_backup.sh
@@ -12,6 +12,10 @@ fi
 for db in $DB_LIST; do
         /www/wdlinux/mysql-5.5.58/bin/mysqldump -uroot -p11111111 --default-character-set=utf8 
 		 --lock-all-tables --master-date=2 --flush-logs --routines --triggers --events --hex-blob --log-error=$backup_dir/$db-error.log --databases $db > $backup_dir/$db.sql
+		 
+		 
+		#/www/wdlinux/mysql/bin/mysqldump -uroot -p1111111 -hlocalhost c_plus --default-character-set=utf8 -R -E | gzip > /www/web/data_backup/mysql_bak/c_plus_`date +%Y%m%d%H%M%S`.sql.tar.gz
+		 
 done
 
 cd $base_dir
